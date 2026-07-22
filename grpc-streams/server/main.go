2d6684de7b691063121dc90474f608e32c4efd84
sai <saivaraprasad072@gmail.com>
2026-07-22 10:52:08 +0530

    generatefibonacci implementation in server grpc : server side streaming

--- grpc-streams/server/main.go
+++ grpc-streams/server/main.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	pb "server/proto/gen"
+	"time"
+)
 
-func main(){
+type server struct {
+	pb.UnimplementedCalculatorServer
+}
+
+func (s *server) GenerateFibonacci(req *pb.FibonacciRequest, stream *pb.Calculator_GenerateFibonacciServer) error {
+	n := req.Number
+	a, b := 0, 1
+
+	for i := 0; i < int(n); i++ {
+		err := stream.Send(&pb.FibonacciResponse{
+			Number: int32(a),
+		})
+		if err != nil {
+			return err
+		}
+		a, b = b, a+b
+		time.Sleep(time.Second * 2)
+	}
+	return nil
+}
+
+func main() {
 
 }
